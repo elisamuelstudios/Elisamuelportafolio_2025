ee5f6ee1fd1c3d443f1c9c98e6c8eeef41a20afe
Elisamuel <elisamuelvalera@gmail.com>
2025-04-05 08:48:17 -0500

    update

--- portafolio/views.py
+++ portafolio/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.contrib import messages
 
 # Create your views here.
 def index(request):
@@ -7,8 +9,36 @@ def index(request):
 def about(request):
     return render(request, 'about.html')
 
-def contact(request):
-    return render(request, 'contact.html')
+def contact (request):
+    if request.method == "POST":
+        nombre = request.POST.get("name")
+        email = request.POST.get("email")
+        asunto = request.POST.get("asunto")
+        mensaje = request.POST.get("message")
+
+        # Formatear el mensaje
+        mensaje_completo = f"""
+        Nombre: {nombre}
+        Email: {email}
+        Asunto: {asunto}
+        
+        Mensaje:
+        {mensaje}
+        """
+
+        # Enviar el correo
+        send_mail(
+            subject=f"Mensaje de Fucaesp.org: {asunto}",
+            message=mensaje_completo,
+            from_email=email,  # Remitente (puede ser el del usuario)
+            recipient_list=["fucaesp@gmail.com"],  # Destinatario
+            fail_silently=False,
+        )
+
+        messages.success(request, "¡Tu mensaje ha sido enviado con éxito!", extra_tags='contacto')
+        return redirect("contacto")  # Redirige a la página de contacto
+
+    return render(request, "contacto.html")  # Asegúrate de tener esta plantilla
 
 def portfolio(request): 
     return render(request, 'portfolio.html')
