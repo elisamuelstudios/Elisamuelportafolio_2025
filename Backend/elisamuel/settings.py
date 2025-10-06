1e98806469b96ba18a7d965f19592ee4674b36fb
Elisamuel Valera Socarrás <98656922+Elisamuel04@users.noreply.github.com>
2025-10-06 12:16:16 -0500

    Update settings.py

--- Backend/elisamuel/settings.py
+++ Backend/elisamuel/settings.py
@@ -109,7 +109,10 @@ WSGI_APPLICATION = 'elisamuel.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
 
 
